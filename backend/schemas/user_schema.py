4762fcd45624a9b8fef2c68dc10e5bdaf1f4b804
Fireolll <pincukt32@gmail.com>
2026-05-20 18:48:32 +0300

    updated /schemes

--- backend/schemas/user_schema.py
+++ backend/schemas/user_schema.py
@@ -1,1 +1,34 @@
 # Схеми користувача.
+
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+from datetime import datetime
+from typing import Optional
+from core.enums import Currency, FinancialPeriod
+
+# Базова схема для користувача
+class UserBase(BaseModel):
+    user_nickname: str = Field(..., min_length=2, max_length=30, description="Нікнейм користувача")
+    email: EmailStr
+    currency: Currency = Currency.UAH
+    financial_period: FinancialPeriod = FinancialPeriod.MONTH
+
+# Схема для обробки інформації з фронтендя для реєстрацій ного користувача
+class UserCreate(UserBase):
+    password: str = Field(..., min_length=8, description="Пароль користувача")
+
+# Схема для оновлень та налаштувань користувача
+# Тут усі поля опціональні, бо користувач може змінити лише нікнейм або лише валюту
+class UserUpdate(BaseModel):
+    user_nickname: Optional[str] = Field(None, min_length=2, max_length=30)
+    currency: Optional[Currency] = None
+    financial_period: Optional[FinancialPeriod] = None
+
+# Схема для надання фронтенду інформації від бека у формі jspn
+class UserResponse(UserBase):
+    user_id: int
+    created_at: datetime
+    updated_at: datetime
+
+    
+    # конвертація з sqlalchemy моделей до pydantic моделей
+    model_config = ConfigDict(from_attributes=True)
