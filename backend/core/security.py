149f1e1c2295b825990c2a0b602340865df6378e
Fireolll <pincukt32@gmail.com>
2026-05-20 23:57:11 +0300

    some more changes

--- backend/core/security.py
+++ backend/core/security.py
@@ -4,6 +4,12 @@ from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
 from jose import jwt
 from core.config import settings
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from jose import JWTError
+from core.database import get_db
+from dal.user_dal import get_user_by_id
 
 # Налаштовуємо алгоритм хешування
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -38,3 +44,30 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     )
     
     return encoded_jwt
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme), 
+    db: AsyncSession = Depends(get_db)
+):
+   #діставання токена з запиту, розшифровка його, і потім пошук юзера в бд
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не вдалося перевірити облікові дані",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id_str: str = payload.get("sub") # type: ignore
+        if user_id_str is None:
+            raise credentials_exception
+        user_id = int(user_id_str)
+    except JWTError:
+        raise credentials_exception
+        
+
+    user = await get_user_by_id(db, user_id)
+    if user is None:
+        raise credentials_exception
+        
+    return user
--- backend/routers/auth_router.py
+++ backend/routers/auth_router.py
@@ -1,1 +1,27 @@
- # Реєстрація, логін, logout та відновлення пароля.
+from fastapi import APIRouter, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.database import get_db
+from schemas.user_schema import UserCreate, UserResponse
+from schemas.token_schema import TokenResponse
+from services import auth_service
+
+#створення самого роутера
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+async def register(user_data: UserCreate, db: AsyncSession = Depends(get_db)):
+   # Реєстрація нового користувача.
+
+    new_user = await auth_service.register_new_user(db, user_data)
+    return new_user
+
+@router.post("/login", response_model=TokenResponse)
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+    #Отримання JWT-токена для доступу до захищених ендпоінтів.
+    token_data = await auth_service.authenticate_user(db, email=form_data.username, password=form_data.password)
+    return token_data
